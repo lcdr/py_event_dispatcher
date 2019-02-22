43bcc8dda1aae73d3eab278cf183039a711f3094
lcdr <lcdr@users.noreply.github.com>
2019-02-22 12:50:59 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+import os.path
+
+from setuptools import setup
+
+setup(
+	name="event_dispatcher",
+	version="0.1.0",
+	description="Simple event dispatcher for indirect calling of listeners.",
+	author="lcdr",
+	url="https://bitbucket.org/lcdr/event_dispatcher/",
+	license="GPL v3",
+	packages=["event_dispatcher"],
+	python_requires=">=3.6",
+)
